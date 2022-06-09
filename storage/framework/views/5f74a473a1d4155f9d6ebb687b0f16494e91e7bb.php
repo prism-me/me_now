25fa07ad23fb3d2f803a6203cd6d2f4aa57cd2f2
tanujaPrism21 <devteam5@prism-me.com>
2022-06-09 15:51:55 +0400

    Changes

--- storage/framework/views/5f74a473a1d4155f9d6ebb687b0f16494e91e7bb.php
+++ storage/framework/views/5f74a473a1d4155f9d6ebb687b0f16494e91e7bb.php
@@ -3,7 +3,7 @@
     <thead>
     </thead>
     <tbody>
-        <h2 style="text-align: center;">Hi <?php echo e(ucfirst($clientMailData['first_name']) . ' ' .ucfirst($clientMailData['last_name'])); ?>, Thank you for your interst in the workshop "<?php echo e($clientMailData['name']); ?>".</h2><br>
+        <h2 style="text-align: center;">Hi <?php echo e(ucfirst( $clientMailData['first_name']) . ' ' .ucfirst( $clientMailData['last_name'])); ?>, Thank you for your interst in the workshop "<?php echo e($clientMailData['name']); ?>".</h2><br>
         <h3 style="text-align: center;">Date: <?php echo e($clientMailData['date']); ?></h3>
         <h4 style="text-align: center;color:rgb(222, 68, 68);">Please click below to confirm your booking.</h4><br>
         <?php $title =  $clientMailData['name']; ?>
